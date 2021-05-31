d646900b6671538cfb4ef03b6bcf12162130ed56
SpectralJager <unkn.spectrum@gmail.com>
2021-05-31 12:16:46 +0700

    update

---
+++ frontend-vue/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <v-app-bar flat dark dense app>
+        <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-lg-and-up"></v-app-bar-nav-icon>
+        <v-app-bar-title>BeyondF1 - Admin panel</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-btn text>
+            <span>Logout</span>
+            <v-icon class="pl-1">mdi-logout</v-icon>
+        </v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" dark class="deep-purple" width = "330">
+        <v-list-item>
+            <v-list-item-content>
+                <v-list-item-title>Admin panel</v-list-item-title>
+                <v-list-item-subtitle>Formula 1 infomation resource</v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+            <v-list-item link v-for="cat in categories" :key="cat.name" :to="cat.path">
+                <v-icon left>{{ cat.icon }}</v-icon>
+                <v-list-item-content>
+                    {{ cat.name }}
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: true,
+            categories: [
+                {name: "Dashboard", icon: "mdi-view-dashboard", path: "/admin/dashboard"},
+                {name: "Articles", icon: "mdi-text-box", path: "/admin/articles"},
+                {name: "Subscribers", icon: "mdi-account-multiple-check", path: "/admin/subscribers"},
+                {name: "Admin settings", icon: "mdi-account-box", path: "/admin/settings"},
+            ]
+        }
+    }
+}
+</script>
---
+++ frontend-vue/src/views/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <v-container fluid>
+  <v-row no-gutters wrap justify="space-between">
+    <v-col md="6" sm="12" class="pa-2">
+      <v-card >
+        <v-card-title>
+          <span class="d-inline-block mr-2">Tasks</span>
+          <v-btn text color="success">Add Task</v-btn>
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-list>
+
+        </v-list>
+      </v-card>
+    </v-col>
+    <v-col md="6" sm="12" class="pa-2">
+      <v-card >
+        <v-card-title>Logs</v-card-title>
+        <v-divider></v-divider>
+        <v-list>
+
+        </v-list>
+      </v-card>
+    </v-col>
+  </v-row>
+
+  </v-container>
+</div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+    data(){
+      return{
+        tab
+      }
+    },
+
+    components: {
+    },
+  }
+</script>
